8144cb899d9df24b079d01355c1d165d6a34dfd1
Samuel Odongo <samieodd@gmail.com>
2024-03-21 08:13:07 +0300

    Add store to scraper initialisation

--- lib/logic/scraper.dart
+++ lib/logic/scraper.dart
@@ -2,8 +2,10 @@ class Scraper {
   final String defaultUrl = 'https://www.google.com/';
   final String ukUrl = 'https://www.google.co.uk/';
   final String searchTerm;
+  final dynamic store;
 
   Scraper({
     required this.searchTerm,
+    required this.store,
   });
 }
