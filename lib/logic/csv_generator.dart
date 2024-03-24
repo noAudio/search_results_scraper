65cd9c92e7036a583fba0c033955b7851ecbba20
Samuel Odongo <samieodd@gmail.com>
2024-03-24 16:58:00 +0300

    Create csv generator class

---
+++ lib/logic/csv_generator.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:search_results/models/search_result.dart';
+
+class CSVGenerator {
+  final String fileName;
+  final List<SearchResult> results;
+
+  CSVGenerator({required this.fileName, required this.results});
+
+  void generate() {
+    // TODO: Save to Documents directory
+    var file = File('$fileName.csv');
+    file.writeAsStringSync(
+        'Keyword, Date, Sponsored, Website Name, URL, Headline Text, Sub-text\n');
+
+    for (var result in results) {
+      // TODO: Format date
+      file.writeAsStringSync(
+        '${result.searchTerm}, ${result.dateTime}, ${result.isSponsored ? "Yes" : "No"}, ${result.website}, ${result.destinationURL}, "${result.headlineText}", ${result.subText}\n',
+        mode: FileMode.append,
+      );
+    }
+  }
+}
